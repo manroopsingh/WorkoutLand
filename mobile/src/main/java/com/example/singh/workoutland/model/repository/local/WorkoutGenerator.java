e959e226559edb3ae3900ae6719f7bdc6155f3b7
ManroopSingh <singh@manroop.com>
2018-05-21 07:58:58 -0400

    added day1 muscle1 ex1 to workout day

--- mobile/src/main/java/com/example/singh/workoutland/model/repository/local/WorkoutGenerator.java
+++ mobile/src/main/java/com/example/singh/workoutland/model/repository/local/WorkoutGenerator.java
@@ -1,7 +1,10 @@
 package com.example.singh.workoutland.model.repository.local;
 
+import com.example.singh.workoutland.model.Exercise;
+import com.example.singh.workoutland.model.ExerciseUnit;
 import com.example.singh.workoutland.model.Workout;
 import com.example.singh.workoutland.model.WorkoutDay;
+import com.example.singh.workoutland.model.WorkoutMuscle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +23,22 @@ public class WorkoutGenerator {
 
         List<WorkoutDay> workoutDays = new ArrayList<>();
 
-        //day 1
-        //WorkoutDay day1 = new WorkoutDay(1)
-        return null;
+        //day 1 SHOULDER
+        List<ExerciseUnit> shoulderExercises = new ArrayList<>();
+        ExerciseUnit ex1 = new ExerciseUnit(new Exercise("Dumbbell Military press", "Desc", "Url"), 8, 8, "Each Set");
+        shoulderExercises.add(ex1);
+
+
+        WorkoutMuscle shoulder = new WorkoutMuscle("Shoulder", "Desc", shoulderExercises);
+
+        List<WorkoutMuscle> day1Muscles = new ArrayList<>();
+        day1Muscles.add(shoulder);
+        WorkoutDay day1 = new WorkoutDay(1, "Shoulder", day1Muscles, "10minutes walks, 5-10 minutes stretching");
+
+        workoutDays.add(day1);
+
+        return workoutDays;
     }
+
 
 }
